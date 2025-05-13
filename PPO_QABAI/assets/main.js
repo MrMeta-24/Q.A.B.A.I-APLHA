829b3a4a92328aed4ba04ed0c5bf056f5f7f03c6
MrMeta-24 <marciobentomanoel@gmail.com>
2025-05-13 14:51:21 -0300

    Add files via upload

---
+++ PPO_QABAI/assets/main.js
@@ -0,0 +1,8 @@
+function toggleAside() {
+    const aside = document.getElementById("asside");
+    aside.classList.toggle("show");
+  }
+  
+  function fecharAside() {
+    document.getElementById("meuAside").classList.remove("show");
+  }
