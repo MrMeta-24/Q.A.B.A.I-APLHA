79da0b4ae7ec5f6f57a89bce34187bcfcdc6685e
MrMeta-24 <marciobentomanoel@gmail.com>
2025-05-27 14:56:00 -0300

    Add files via upload

---
+++ Q.A.B.A.I-APLHA-main/PPO_QABAI/inicio-assets/inicio.js
@@ -0,0 +1,19 @@
+/*
+
+function toggleAside() {
+    const aside = document.getElementById("asside");
+    aside.classList.toggle("show");
+  }
+  
+  function fecharAside() {
+    document.getElementById("meuAside").classList.remove("show");
+  }
+  */
+
+  function toggleAside() {
+    document.getElementById("asside").classList.toggle("open");
+}
+
+function fecharAside() {
+    document.getElementById("asside").classList.remove("open");
+}
